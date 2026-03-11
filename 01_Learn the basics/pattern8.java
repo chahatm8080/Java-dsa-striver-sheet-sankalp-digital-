4f02cb51ebd0af19cb2799651f4a2772b3947a8c
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-03-10 22:11:54 -0500

    Pattern - 8: Inverted Star Pyramid

--- 01_Learn the basics/pattern8.java
+++ 01_Learn the basics/pattern8.java
@@ -1,16 +1,16 @@
-class pattern7 {
+class pattern8 {
     public void pattern(int N) {
         for (int i = 0; i < N; i++) {
             // Print leading spaces
-            for (int j = 0; j < N - i - 1; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             // Print stars
-            for (int j = 0; j < 2 * i + 1; j++) {
+            for (int j = 0; j < N * 2 - (2 * i + 1); j++) {
                 System.out.print("*");
             }
             // Print trailing spaces
-            for (int j = 0; j < N - i - 1; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             System.out.println();
@@ -19,7 +19,7 @@ class pattern7 {
 
     public static void main(String[] args) {
         // Creating object of Solution class
-        pattern7 sol = new pattern7();
+        pattern8 sol = new pattern8();
 
         int N = 5;
 
