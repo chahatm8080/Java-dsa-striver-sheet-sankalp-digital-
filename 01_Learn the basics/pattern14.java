b6aff467fde5f9d2b2021cce3986e017eac7354b
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-03-10 22:22:49 -0500

    Pattern-14: Increasing Letter Triangle Pattern

---
+++ 01_Learn the basics/pattern14.java
@@ -0,0 +1,24 @@
+class pattern14 {
+    public void pattern(int N) {
+        for (int i = 0; i < N; i++) {
+
+            // Inner loop to print alphabets from A to A + i
+            for (char ch = 'A'; ch <= 'A' + i; ch++) {
+                System.out.print(ch + " "); // Print the character followed by a space
+            }
+
+            // Move to the next line after printing the current row
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Creating object of Solution class
+        pattern14 sol = new pattern14();
+
+        int N = 5;
+
+        // Calling pattern function
+        sol.pattern(N);
+    }
+}
