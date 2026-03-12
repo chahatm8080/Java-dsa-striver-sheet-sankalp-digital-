a954c4dfd75f0478a0c64e204b84c2f7e69aab20
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-03-12 15:55:11 -0500

    Pattern-19: Symmetric-Void Pattern

---
+++ 01_Learn the basics/pattern28.java
@@ -0,0 +1,37 @@
+public class pattern28 {
+  static void pattern(int n) {
+    int iniS = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print("*");
+      }
+      for (int j = 0; j < iniS; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print("*");
+      }
+      iniS += 2;
+      System.out.println();
+    }
+    iniS = 2 * n - 2;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      for (int j = 0; j < iniS; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      iniS -= 2;
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int n = 4;
+    pattern(n);
+  }
+}
