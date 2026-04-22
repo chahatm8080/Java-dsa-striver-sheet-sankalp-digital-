430b489bef92806298c7bc7f2d8ce91baf9bb579
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-22 09:57:40 -0500

    revising bubble sort

--- 02_Learn Important Sorting Techniques/bubble_sort54.java
+++ 02_Learn Important Sorting Techniques/bubble_sort54.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class bubble_sort54 {
-  public static void Bubble_sort(int[] arr, int n) {
-    for (int i = n - 1; i > 0; i--) {
+class bubble_sort54 {
+  public static void bubbleSort(int[] arr, int n) {
+    for (int i = n - 1; i >= 0; i--) {
       int maxi = i;
-      for (int j = 0; j <= i - 1; j++) {
+      for (int j = 0; j < i; j++) {
         if (arr[j] > arr[maxi]) {
           maxi = j;
         }
       }
       int temp = arr[maxi];
+      arr[maxi] = arr[i];
       arr[i] = temp;
     }
-    System.out.print("BUBBLE SORT DONE: ");
     for (int num : arr) {
       System.out.print(num + " ");
     }
@@ -23,11 +23,11 @@ public class bubble_sort54 {
     System.out.println("Enter size of array");
     int n = sc.nextInt();
     int arr[] = new int[n];
-    System.out.println("Enter elements in array");
+    System.out.println("Enter array elements: ");
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
-    Bubble_sort(arr, n);
+    bubbleSort(arr, n);
     sc.close();
   }
 }
