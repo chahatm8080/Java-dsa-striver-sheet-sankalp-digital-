bcc00a68e7fa9cd505f7e3e338d6522aa89cc431
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-22 09:45:05 -0500

    adding selection

---
+++ 02_Learn Important Sorting Techniques/selection_sort53.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class selection_sort53 {
+  public static void selSort(int[] arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+      int mini = i;
+      for (int j = i + 1; j < n; j++) {
+        if (arr[j] < arr[mini]) {
+          mini = j;
+        }
+      }
+      int temp = arr[mini];
+      arr[mini] = arr[i];
+      arr[i] = temp;
+    }
+    for (int num : arr) {
+      System.out.print(num + " ");
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter size of arrays");
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    System.out.println("Enter elements in a given array");
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    selSort(arr, n);
+    sc.close();
+  }
+}
