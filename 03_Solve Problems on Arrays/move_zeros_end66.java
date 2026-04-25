82bfdb6e3d620441a61cee2085082c125993c566
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-25 17:02:25 -0500

    Optimal Approach

--- 03_Solve Problems on Arrays/move_zeros_end66.java
+++ 03_Solve Problems on Arrays/move_zeros_end66.java
@@ -2,20 +2,41 @@ import java.util.Scanner;
 
 class move_zeros_end66 {
   public static void move_zeros_end(int[] arr, int n) {
-    int[] temp = new int[arr.length];
-    int index = 0;
+    // int[] temp = new int[arr.length];
+    // int index = 0;
+    // for (int i = 0; i < n; i++) {
+    // if (arr[i] != 0) {
+    // temp[index] = arr[i];
+    // index++;
+    // }
+    // }
+    // for (int i = 0; i < temp.length; i++) {
+    // arr[i] = temp[i];
+    // }
+    // for (int num : arr) {
+    // System.out.print(num + " ");
+    // }
+    int j = -1;
     for (int i = 0; i < n; i++) {
-      if (arr[i] != 0) {
-        temp[index] = arr[i];
-        index++;
+      if (arr[i] == 0) {
+        j = i;
+        break;
       }
     }
-    for (int i = 0; i < temp.length; i++) {
-      arr[i] = temp[i];
+    if (j == -1)
+      return;
+    for (int i = j + 1; i < n; i++) {
+      if (arr[i] != 0) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j++;
+      }
     }
     for (int num : arr) {
       System.out.print(num + " ");
     }
+
   }
 
   public static void main(String[] args) {
