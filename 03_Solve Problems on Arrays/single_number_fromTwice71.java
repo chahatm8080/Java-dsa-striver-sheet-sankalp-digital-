89ad23018cc1d0998d37e522f7d59effd0a24452
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-26 17:19:50 -0500

    using xor operations

--- 03_Solve Problems on Arrays/single_number_fromTwice71.java
+++ 03_Solve Problems on Arrays/single_number_fromTwice71.java
@@ -1,35 +1,41 @@
 class Solution {
   // Function to find the single element using a hash array
   public int getSingleElement(int[] arr) {
+    // int n = arr.length;
+
+    // // Step 1: Find maximum element
+    // int maxi = arr[0];
+    // for (int i = 0; i < n; i++) {
+    // maxi = Math.max(maxi, arr[i]);
+    // }
+
+    // // Step 2: Create frequency array of size maxi+1
+    // int[] hash = new int[maxi + 1];
+
+    // // Step 3: Count frequencies
+    // for (int i = 0; i < n; i++) {
+    // hash[arr[i]]++;
+    // }
+
+    // // Step 4: Find element with frequency = 1
+    // for (int i = 0; i < n; i++) {
+    // if (hash[arr[i]] == 1)
+    // return arr[i];
+    // }
+
+    // return -1; // fallback
     int n = arr.length;
-
-    // Step 1: Find maximum element
-    int maxi = arr[0];
+    int xorr = 0;
     for (int i = 0; i < n; i++) {
-      maxi = Math.max(maxi, arr[i]);
+      xorr ^= arr[i];
     }
-
-    // Step 2: Create frequency array of size maxi+1
-    int[] hash = new int[maxi + 1];
-
-    // Step 3: Count frequencies
-    for (int i = 0; i < n; i++) {
-      hash[arr[i]]++;
-    }
-
-    // Step 4: Find element with frequency = 1
-    for (int i = 0; i < n; i++) {
-      if (hash[arr[i]] == 1)
-        return arr[i];
-    }
-
-    return -1; // fallback
+    return xorr;
   }
 }
 
 class single_number_fromTwice71 {
   public static void main(String[] args) {
-    int[] arr = { 4, 1, 2, 1, 2 };
+    int[] arr = { 4, 1, 2, 1, 2, 5 };
     Solution obj = new Solution();
     int ans = obj.getSingleElement(arr);
     System.out.println("The single element is: " + ans);
