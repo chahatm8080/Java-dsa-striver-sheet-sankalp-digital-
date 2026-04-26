2ce43938f65e33d12ae38900cd3818614b71ee4e
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-26 12:01:02 -0500

    using set: union sorted array

--- 03_Solve Problems on Arrays/union_sorted_array68.java
+++ 03_Solve Problems on Arrays/union_sorted_array68.java
@@ -1,23 +1,38 @@
 import java.util.*;
 
-class union_sorted_array68 {
-  public static List<Integer> union_sorted_array(int[] nums, int[] nums2, int n1, int n2) {
-    Map<Integer, Integer> mpp = new HashMap<>();
+class Solution {
+  public List<Integer> union_sorted_array(int[] nums, int[] nums2, int n1, int n2) {
+    Set<Integer> st = new TreeSet<>();
     for (int i = 0; i < n1; i++) {
-      mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+      st.add(nums[i]);
     }
     for (int i = 0; i < n2; i++) {
-      mpp.put(nums2[i], mpp.getOrDefault(nums[i], 0) + 1);
+      st.add(nums[i]);
     }
-    List<Integer> union = new ArrayList<>();
-    for (int num : mpp.keySet()) {
-      union.add(num);
-    }
-    return union;
+    return new ArrayList<>(st);
   }
+}
+
+class union_sorted_array68 {
+  // public static List<Integer> union_sorted_array(int[] nums, int[] nums2, int
+  // n1, int n2) {
+  // Map<Integer, Integer> mpp = new HashMap<>();
+  // for (int i = 0; i < n1; i++) {
+  // mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+  // }
+  // for (int i = 0; i < n2; i++) {
+  // mpp.put(nums2[i], mpp.getOrDefault(nums[i], 0) + 1);
+  // }
+  // List<Integer> union = new ArrayList<>();
+  // for (int num : mpp.keySet()){
+  // union.add(num);
+  // }
+  // return union;
+  // }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    Solution obj = new Solution();
     System.out.println("Enter n1 Size of array");
     int n1 = sc.nextInt();
     int arr[] = new int[n1];
@@ -32,7 +47,7 @@ class union_sorted_array68 {
     for (int i = 0; i < n2; i++) {
       arr2[i] = sc.nextInt();
     }
-    List<Integer> result = union_sorted_array(arr, arr2, n1, n2);
+    List<Integer> result = obj.union_sorted_array(arr, arr2, n1, n2);
     for (int val : result)
       System.out.print(val + " ");
     sc.close();
