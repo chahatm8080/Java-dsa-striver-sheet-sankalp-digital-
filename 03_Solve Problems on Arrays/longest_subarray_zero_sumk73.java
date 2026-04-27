fa2bd4104622609877c11958fdf128a148112ce0
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-27 16:54:22 -0500

    optimal longest subarray with given sum zero

--- 03_Solve Problems on Arrays/longest_subarray_zero_sumk73.java
+++ 03_Solve Problems on Arrays/longest_subarray_zero_sumk73.java
@@ -2,14 +2,31 @@ import java.util.*;
 
 class Solution {
   public int longest_subarraysum(int[] arr) {
+    // int n = arr.length;
+    // int maxi = 0;
+    // for (int i = 0; i < n - 1; i++) {
+    // int sum = arr[i];
+    // for (int j = i + 1; j < n; j++) {
+    // sum += arr[j];
+    // if (sum == 0) {
+    // maxi = Math.max(maxi, j - i + 1);
+    // }
+    // }
+    // }
+    // return maxi;
     int n = arr.length;
+    Map<Integer, Integer> mpp = new HashMap<>();
     int maxi = 0;
-    for (int i = 0; i < n - 1; i++) {
-      int sum = arr[i];
-      for (int j = i + 1; j < n; j++) {
-        sum += arr[j];
-        if (sum == 0) {
-          maxi = Math.max(maxi, j - i + 1);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += arr[i];
+      if (sum == 0) {
+        maxi = i + 1;
+      } else {
+        if (mpp.containsKey(sum)) {
+          maxi = Math.max(maxi, i - mpp.get(sum));
+        } else {
+          mpp.put(sum, i);
         }
       }
     }
