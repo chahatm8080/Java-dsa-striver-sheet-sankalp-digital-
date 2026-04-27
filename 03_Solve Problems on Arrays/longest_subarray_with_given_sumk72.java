5daaa1a27c61e6c53fb01388e73085dcf58a2afd
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-27 11:08:01 -0500

    bruteforce longest subarray with given sum k

---
+++ 03_Solve Problems on Arrays/longest_subarray_with_given_sumk72.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Solution {
+  public int longest_subarraysum(int[] arr, int n, int k) {
+    int maxi = 0;
+    for (int i = 0; i < n - 1; i++) {
+      int sum = arr[i];
+      for (int j = i + 1; j < n; j++) {
+        sum += arr[j];
+        if (sum == k) {
+          maxi = j - i + 1;
+        }
+      }
+    }
+    return maxi;
+  }
+}
+
+class longest_subarray_with_given_sumk72 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Solution sol = new Solution();
+    System.out.println("Enter size of array:");
+    int n = sc.nextInt();
+    int k = 15;
+    int arr[] = new int[n];
+    System.out.println("Enter elements in array");
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    System.out.println("Longest subarray is: ");
+    System.out.print(sol.longest_subarraysum(arr, n, k));
+    sc.close();
+  }
+}
