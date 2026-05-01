466be7a78e56592fa830599602f406284cfe02c9
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-04-30 20:58:48 -0500

    optimal : two pointer approach

--- 03_Solve Problems on Arrays/union_sorted_array68.java
+++ 03_Solve Problems on Arrays/union_sorted_array68.java
@@ -1,17 +1,67 @@
 import java.util.*;
 
 class Solution {
-  public List<Integer> union_sorted_array(int[] nums, int[] nums2, int n1, int n2) {
-    Set<Integer> st = new TreeSet<>();
-    for (int i = 0; i < n1; i++) {
-      st.add(nums[i]);
+  public List<Integer> union_sorted_array(int[] arr1, int[] arr2, int n, int m) {
+    List<Integer> Union = new ArrayList<>();
+
+    // Initialize pointers
+    int i = 0, j = 0;
+
+    // Iterate while both arrays have elements
+    while (i < n && j < m) {
+      // If element in arr1 is smaller
+      if (arr1[i] < arr2[j]) {
+        // Add if empty or not duplicate
+        if (Union.isEmpty() || Union.get(Union.size() - 1) != arr1[i])
+          Union.add(arr1[i]);
+        i++; // Move pointer in arr1
+      }
+      // If element in arr2 is smaller
+      else if (arr2[j] < arr1[i]) {
+        // Add if empty or not duplicate
+        if (Union.isEmpty() || Union.get(Union.size() - 1) != arr2[j])
+          Union.add(arr2[j]);
+        j++; // Move pointer in arr2
+      } else {
+        // Elements are equal, add once if not duplicate
+        if (Union.isEmpty() || Union.get(Union.size() - 1) != arr1[i])
+          Union.add(arr1[i]);
+        i++;
+        j++; // Move both pointers
+      }
     }
-    for (int i = 0; i < n2; i++) {
-      st.add(nums[i]);
+
+    // Append remaining elements from arr1
+    while (i < n) {
+      if (Union.isEmpty() || Union.get(Union.size() - 1) != arr1[i])
+        Union.add(arr1[i]);
+      i++;
     }
-    return new ArrayList<>(st);
+
+    // Append remaining elements from arr2
+    while (j < m) {
+      if (Union.isEmpty() || Union.get(Union.size() - 1) != arr2[j])
+        Union.add(arr2[j]);
+      j++;
+    }
+
+    // Return the union list
+    return Union;
   }
 }
+// class Solution {
+// public List<Integer> union_sorted_array(int[] nums, int[] nums2, int n1, int
+// n2) {
+// Set<Integer> st = new TreeSet<>();
+// for (int i = 0; i < n1; i++) {
+// st.add(nums[i]);
+// }
+// for (int i = 0; i < n2; i++) {
+// st.add(nums[i]);
+// }
+// return new ArrayList<>(st);
+// }
+// }
 
 class union_sorted_array68 {
   // public static List<Integer> union_sorted_array(int[] nums, int[] nums2, int
