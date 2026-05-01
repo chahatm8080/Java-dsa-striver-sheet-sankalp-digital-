a2e94a636d3de4b50543ef05d5af5821a428862a
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-01 12:00:18 -0500

    revising

--- 02_Learn Important Sorting Techniques/bubble_sort54.java
+++ 02_Learn Important Sorting Techniques/bubble_sort54.java
@@ -1,24 +1,43 @@
 import java.util.Scanner;
 
-class bubble_sort54 {
-  public static void bubbleSort(int[] arr, int n) {
+class Solution {
+  // public static void bubbleSort(int[] arr, int n) {
+  // for (int i = n - 1; i >= 0; i--) {
+  // int maxi = i;
+  // for (int j = 0; j < i; j++) {
+  // if (arr[j] > arr[maxi]) {
+  // maxi = j;
+  // }
+  // }
+  // int temp = arr[maxi];
+  // arr[maxi] = arr[i];
+  // arr[i] = temp;
+  // }
+  // for (int num : arr) {
+  // System.out.print(num + " ");
+  // }
+  public void bubbleSort(int[] arr) {
+    int n = arr.length;
     for (int i = n - 1; i >= 0; i--) {
       int maxi = i;
       for (int j = 0; j < i; j++) {
         if (arr[j] > arr[maxi]) {
           maxi = j;
         }
+        int temp = arr[maxi];
+        arr[maxi] = arr[i];
+        arr[i] = temp;
       }
-      int temp = arr[maxi];
-      arr[maxi] = arr[i];
-      arr[i] = temp;
     }
     for (int num : arr) {
       System.out.print(num + " ");
     }
   }
+}
 
+class bubble_sort54 {
   public static void main(String[] args) {
+    Solution sol = new Solution();
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter size of array");
     int n = sc.nextInt();
@@ -27,7 +46,7 @@ class bubble_sort54 {
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
-    bubbleSort(arr, n);
+    sol.bubbleSort(arr);
     sc.close();
   }
 }
