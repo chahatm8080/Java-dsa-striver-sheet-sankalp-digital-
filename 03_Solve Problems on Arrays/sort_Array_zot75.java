8c2f0db488cc74d01f29e7857e4ebc609c77f659
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-02 12:55:36 -0500

    optimal2 : dutch national flag

--- 03_Solve Problems on Arrays/sort_Array_zot75.java
+++ 03_Solve Problems on Arrays/sort_Array_zot75.java
@@ -2,25 +2,48 @@ import java.util.Scanner;
 
 class Solution {
   public void sortColor(int[] arr) {
+    // int n = arr.length;
+    // int count0 = 0, count1 = 0, count2 = 0;
+    // for (int i = 0; i < n; i++) {
+    // if (arr[i] == 0) {
+    // count0++;
+    // } else if (arr[i] == 1) {
+    // count1++;
+    // } else {
+    // count2++;
+    // }
+    // }
+    // for (int i = 0; i < count0; i++) {
+    // arr[i] = 0;
+    // }
+    // for (int i = count0; i < count0 + count1; i++) {
+    // arr[i] = 1;
+    // }
+    // for (int i = count0 + count1; i < count0 + count1 + count2; i++) {
+    // arr[i] = 2;
+    // }
+    // for (int num : arr) {
+    // System.out.print(num + " ");
+    // }
     int n = arr.length;
-    int count0 = 0, count1 = 0, count2 = 0;
-    for (int i = 0; i < n; i++) {
-      if (arr[i] == 0) {
-        count0++;
-      } else if (arr[i] == 1) {
-        count1++;
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+    while (mid <= high) {
+      if (arr[mid] == 0) {
+        int temp = arr[mid];
+        arr[mid] = arr[low];
+        arr[low] = temp;
+        low++;
+        mid++;
+      } else if (arr[mid] == 1) {
+        mid++;
       } else {
-        count2++;
+        int temp = arr[mid];
+        arr[mid] = arr[high];
+        arr[high] = temp;
+        high--;
       }
-    }
-    for (int i = 0; i < count0; i++) {
-      arr[i] = 0;
-    }
-    for (int i = count0; i < count0 + count1; i++) {
-      arr[i] = 1;
-    }
-    for (int i = count0 + count1; i < count0 + count1 + count2; i++) {
-      arr[i] = 2;
     }
     for (int num : arr) {
       System.out.print(num + " ");
