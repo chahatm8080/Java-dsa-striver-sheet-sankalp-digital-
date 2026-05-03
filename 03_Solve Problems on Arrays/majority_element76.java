2143da9284167e387869586b96d8a9d3c2665512
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-03 11:17:57 -0500

    Better: hash map

--- 03_Solve Problems on Arrays/majority_element76.java
+++ 03_Solve Problems on Arrays/majority_element76.java
@@ -1,17 +1,30 @@
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
 
 class Solution {
   public int majElement(int[] arr) {
+    // int n = arr.length;
+    // for (int i = 0; i < n; i++) {
+    // int count = 0;
+    // for (int j = 0; j < n; j++) {
+    // if (arr[j] == arr[i]) {
+    // count++;
+    // }
+    // }
+    // if (count > n / 2) {
+    // return arr[i];
+    // }
+    // }
+    // return -1;
     int n = arr.length;
-    for (int i = 0; i < n; i++) {
-      int count = 0;
-      for (int j = 0; j < n; j++) {
-        if (arr[j] == arr[i]) {
-          count++;
-        }
-      }
-      if (count > n / 2) {
-        return arr[i];
+    Map<Integer, Integer> mpp = new HashMap<>();
+    for (int num : arr) {
+      mpp.put(num, mpp.getOrDefault(num, 0) + 1);
+    }
+    for (Map.Entry<Integer, Integer> entry : mpp.entrySet()) {
+      if (entry.getValue() > n / 2) {
+        return entry.getKey();
       }
     }
     return -1;
