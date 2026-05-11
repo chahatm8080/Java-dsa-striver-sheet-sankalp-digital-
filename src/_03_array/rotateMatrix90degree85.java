c54932dd5d5efceef00e914b5ebae996e9a583b1
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-11 13:30:35 -0500

    optimal: transpose and reverse

---
+++ src/_03_array/rotateMatrix90degree85.java
@@ -0,0 +1,37 @@
+class rotate {
+  public void setZeroes(int[][] matrix) {
+    int n = matrix.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      int left = 0;
+      int right = n - 1;
+      while (left < right) {
+        int temp = matrix[i][left];
+        matrix[i][left] = matrix[i][right];
+        matrix[i][right] = temp;
+        left++;
+        right--;
+      }
+    }
+  }
+}
+
+class rotateMatrix90degree85 {
+  public static void main(String[] args) {
+    rotate obj = new rotate();
+    int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    obj.setZeroes(matrix);
+    for (int[] row : matrix) {
+      for (int val : row) {
+        System.out.print(val + " ");
+      }
+      System.out.println();
+    }
+  }
+}
