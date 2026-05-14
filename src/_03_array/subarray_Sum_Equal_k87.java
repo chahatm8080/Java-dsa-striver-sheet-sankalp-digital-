b156842a13698454877ca2ae90b79fca969ac959
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-14 15:38:03 -0500

    opimal: prefixSum method

--- src/_03_array/subarray_Sum_Equal_k87.java
+++ src/_03_array/subarray_Sum_Equal_k87.java
@@ -1,21 +1,36 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 class subarray {
   public int subSumEqlk(int[] arr, int k) {
+    // int n = arr.length;
+    // int count = 0;
+    // for (int i = 0; i < n; i++) {
+    // int sum = 0;
+    // for (int j = i; j < n; j++) {
+    // sum += arr[j];
+    // if (sum == k) {
+    // count++;
+    // }
+    // }
+    // }
+    // return count;
+    // ----------------------optimal approach
     int n = arr.length;
+    HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
+    int prefixSum = 0;
     int count = 0;
+    prefixSumCount.put(0, 1);
     for (int i = 0; i < n; i++) {
-      int sum = 0;
-      for (int j = i; j < n; j++) {
-        sum += arr[j];
-        if (sum == k) {
-          count++;
-        }
+      prefixSum += arr[i];
+      int remove = prefixSum - k;
+      if (prefixSumCount.containsKey(remove)) {
+        count += prefixSumCount.get(remove);
       }
+      prefixSumCount.put(prefixSum, prefixSumCount.getOrDefault(prefixSum, 0) + 1);
     }
     return count;
   }
-
 }
 
 class subarray_Sum_Equal_k87 {
