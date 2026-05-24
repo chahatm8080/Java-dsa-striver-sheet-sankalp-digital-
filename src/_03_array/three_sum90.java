73e7fb2fde5992d42fe70f1d8e311bffb12c5d49
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-24 16:26:45 -0500

    optimal: two pointer

--- src/_03_array/three_sum90.java
+++ src/_03_array/three_sum90.java
@@ -2,21 +2,47 @@ import java.util.*;
 
 class three {
   public List<List<Integer>> threeSum(int[] arr) {
-    int target = 0;
-    Set<List<Integer>> st = new HashSet<>();
+    // int target = 0;
+    // Set<List<Integer>> st = new HashSet<>();
+    // --------------------------------------brute force O(N^4)
+    // int n = arr.length;
+    // for (int i = 0; i < n; i++) {
+    // for (int j = i + 1; j < n; j++) {
+    // for (int k = j + 1; k < n; k++) {
+    // if (arr[i] + arr[j] + arr[k] == target) {
+    // List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+    // Collections.sort(temp);
+    // st.add(temp);
+    // }
+    // }
+    // }
+    // }
+    // return new ArrayList<>(st);
+    Arrays.sort(arr);
+    List<List<Integer>> ls = new ArrayList<>();
     int n = arr.length;
     for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        for (int k = j + 1; k < n; k++) {
-          if (arr[i] + arr[j] + arr[k] == target) {
-            List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
-            Collections.sort(temp);
-            st.add(temp);
-          }
-        }
+      if (i > 0 && arr[i] == arr[i - 1])
+        continue;
+      int left = i + 1;
+      int right = n - 1;
+      while (left < right) {
+        int sum = arr[i] + arr[left] + arr[right];
+        if (sum == 0) {
+          ls.add(Arrays.asList(arr[i], arr[left], arr[right]));
+          left++;
+          right--;
+          while (left < right && arr[left] == arr[left - 1])
+            left++;
+          while (left < right && arr[right] == arr[right + 1])
+            right--;
+        } else if (sum < 0)
+          left++;
+        else
+          right--;
       }
     }
-    return new ArrayList<>(st);
+    return new ArrayList<>(ls);
   }
 }
 
