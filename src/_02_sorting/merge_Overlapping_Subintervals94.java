6ffe5132abafb60d7a8bcced514da4e4cec0c6b9
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-26 13:58:03 -0500

    optimal approach

--- src/_02_sorting/merge_Overlapping_Subintervals94.java
+++ src/_02_sorting/merge_Overlapping_Subintervals94.java
@@ -1,20 +1,38 @@
 import java.util.*;
 
 class subintervals {
-  public List<List<Integer>> merge(int[][] intervals) {
-    List<List<Integer>> ans = new ArrayList<>();
-    int n = intervals.length;
-    int i = 0;
-    while (i < n) {
-      int s = intervals[i][0];
-      int e = intervals[i][1];
-      int j = i + 1;
-      while (j < n && intervals[j][0] <= e) {
-        e = Math.max(e, intervals[j][1]);
-        j++;
+  public int[][] merge(int[][] intervals) {
+    // List<List<Integer>> ans = new ArrayList<>();
+    // int n = intervals.length;
+    // int i = 0;
+    // while (i < n) {
+    // int s = intervals[i][0];
+    // int e = intervals[i][1];
+    // int j = i + 1;
+    // while (j < n && intervals[j][0] <= e) {
+    // e = Math.max(e, intervals[j][1]);
+    // j++;
+    // }
+    // ans.add(Arrays.asList(s, e));
+    // i = j;
+    // }
+    // return ans;
+    // ------------------------optimal
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+    List<List<Integer>> merged = new ArrayList<>();
+    for (int[] interval : intervals) {
+      if (merged.isEmpty() || merged.get(merged.size() - 1).get(1) < interval[0]) {
+        merged.add(Arrays.asList(interval[0], interval[1]));
+      } else {
+        int last = merged.size() - 1;
+        int max_End = Math.max(merged.get(last).get(1), interval[1]);
+        merged.get(last).set(1, max_End);
       }
-      ans.add(Arrays.asList(s, e));
-      i = j;
+    }
+    int[][] ans = new int[merged.size()][2];
+    for (int i = 0; i < merged.size(); i++) {
+      ans[i][0] = merged.get(i).get(0);
+      ans[i][1] = merged.get(i).get(1);
     }
     return ans;
   }
@@ -31,9 +49,9 @@ class merge_Overlapping_Subintervals94 {
       intervals[i][0] = sc.nextInt();
       intervals[i][1] = sc.nextInt();
     }
-    List<List<Integer>> result = sol.merge(intervals);
-    for (List<Integer> interval : result) {
-      System.out.print(interval + " ");
+    int[][] result = sol.merge(intervals);
+    for (int[] interval : result) {
+      System.out.print(Arrays.toString(interval) + " ");
     }
     sc.close();
   }
