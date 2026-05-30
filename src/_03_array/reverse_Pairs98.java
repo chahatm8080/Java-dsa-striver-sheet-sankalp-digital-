ad24774172bb026ddf76fe9f99da75d44881b11f
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-05-30 13:08:39 -0500

    optimal: using merge sort

--- src/_03_array/reverse_Pairs98.java
+++ src/_03_array/reverse_Pairs98.java
@@ -1,18 +1,48 @@
-import java.util.Scanner;
+import java.util.*;
 
 class pairs {
-  public int reversePairs(int[] arr) {
-    int n = arr.length;
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        if (arr[i] > 2 * arr[j]) {
-          count++;
-        } else
-          continue;
-      }
+  public void merge(int[] arr, int low, int mid, int high) {
+    List<Integer> temp = new ArrayList<>();
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high) {
+      if (arr[left] <= arr[right]) {
+        temp.add(arr[left++]);
+      } else
+        temp.add(arr[right++]);
     }
-    return count;
+    while (left <= mid) {
+      temp.add(arr[left++]);
+    }
+    while (right <= high) {
+      temp.add(arr[right++]);
+    }
+    for (int i = low; i <= high; i++) {
+      arr[i] = temp.get(i - low);
+    }
+  }
+
+  public int mergePairs(int[] arr, int low, int mid, int high) {
+    int right = mid + 1;
+    int cnt = 0;
+    for (int i = low; i <= mid; i++) {
+      while (right <= high && arr[i] > 2L * arr[right])
+        right++;
+      cnt += right - (mid + 1);
+    }
+    return cnt;
+  }
+
+  public int mergeSort(int[] arr, int low, int high) {
+    int cnt = 0;
+    if (low >= high)
+      return cnt;
+    int mid = (low + high) / 2;
+    cnt += mergeSort(arr, low, mid);
+    cnt += mergeSort(arr, mid + 1, high);
+    cnt += mergePairs(arr, low, mid, high);
+    merge(arr, low, mid, high);
+    return cnt;
   }
 }
 
@@ -27,7 +57,7 @@ class reverse_Pairs98 {
       arr[i] = sc.nextInt();
     }
     System.out.println("reverse pairs is: ");
-    System.out.println(sol.reversePairs(arr));
+    System.out.println(sol.mergeSort(arr, 0, n - 1));
     sc.close();
   }
 }
