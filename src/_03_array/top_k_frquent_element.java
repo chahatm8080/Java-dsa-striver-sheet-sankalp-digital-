6d99de3d7db0b597641bb55407d80a6dc9ed3f81
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-06-04 11:44:33 -0500

    optimal using bucket sort

--- src/_03_array/top_k_frquent_element.java
+++ src/_03_array/top_k_frquent_element.java
@@ -3,42 +3,66 @@ import java.util.*;
 class top {
 
   public int[] topkFrequent(int[] nums, int k) {
+    // bruteforce approach
+    // Map<Integer, Integer> map = new HashMap<>();
 
-    Map<Integer, Integer> map = new HashMap<>();
+    // for (int num : nums) {
+    // map.put(num, map.getOrDefault(num, 0) + 1);
+    // }
 
-    for (int num : nums) {
-      map.put(num, map.getOrDefault(num, 0) + 1);
+    // int[] result = new int[k];
+
+    // for (int i = 0; i < k; i++) {
+
+    // int maxFreq = 0;
+    // int element = 0;
+
+    // for (int key : map.keySet()) {
+
+    // if (map.get(key) > maxFreq) {
+    // maxFreq = map.get(key);
+    // element = key;
+    // }
+    // }
+
+    // result[i] = element;
+
+    // map.remove(element);
+    // }
+
+    // return result;
+    // optimal approach
+    @SuppressWarnings("unchecked")
+    List<Integer>[] bucket = new List[nums.length + 1];
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
+    for (int n : nums) {
+      frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
     }
-
-    int[] result = new int[k];
-
-    for (int i = 0; i < k; i++) {
-
-      int maxFreq = 0;
-      int element = 0;
-
-      for (int key : map.keySet()) {
-
-        if (map.get(key) > maxFreq) {
-          maxFreq = map.get(key);
-          element = key;
+    for (int key : frequencyMap.keySet()) {
+      int frequency = frequencyMap.get(key);
+      if (bucket[frequency] == null) {
+        bucket[frequency] = new ArrayList<>();
+      }
+      bucket[frequency].add(key);
+    }
+    int[] res = new int[k];
+    int counter = 0;
+    for (int pos = bucket.length - 1; pos >= 0 && counter < k; pos--) {
+      if (bucket[pos] != null) {
+        for (Integer integer : bucket[pos]) {
+          res[counter++] = integer;
         }
       }
-
-      result[i] = element;
-
-      map.remove(element);
     }
-
-    return result;
+    return res;
   }
 }
 
 public class top_k_frquent_element {
   public static void main(String[] args) {
-    int[] arr = { 1 };
+    int[] arr = { 1, 1, 1, 3, 3, 3, 3, 4, 4 };
     top obj = new top();
-    int k = 1;
+    int k = 2;
     int[] res = obj.topkFrequent(arr, k);
     for (int num : res) {
       System.out.print(num + " ");
