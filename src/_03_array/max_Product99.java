ab16478ef2463bbd694bcc007a06323a39842e8d
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-06-09 14:53:20 -0500

    optimal: maximum product

--- src/_03_array/max_Product99.java
+++ src/_03_array/max_Product99.java
@@ -2,20 +2,36 @@ import java.util.Scanner;
 
 class maxPro {
   public int maxProduct(int[] nums) {
-    int maxProd = nums[0];
+    // int maxProd = nums[0];
 
-    for (int i = 0; i < nums.length; i++) {
+    // for (int i = 0; i < nums.length; i++) {
 
-      int prod = 1;
+    // int prod = 1;
 
-      for (int j = i; j < nums.length; j++) {
+    // for (int j = i; j < nums.length; j++) {
 
-        prod *= nums[j];
-        maxProd = Math.max(maxProd, prod);
-      }
+    // prod *= nums[j];
+    // maxProd = Math.max(maxProd, prod);
+    // }
+    // }
+
+    // return maxProd;
+
+    // --------------------------------optimal
+    int n = nums.length;
+    int prefix = 1;
+    int suffix = 1;
+    int ans = Integer.MIN_VALUE;
+    for (int i = 0; i < n; i++) {
+      if (prefix == 0)
+        prefix = 1;
+      if (suffix == 0)
+        suffix = 1;
+      prefix *= nums[i];
+      suffix *= nums[n - i - 1];
+      ans = Math.max(ans, Math.max(prefix, suffix));
     }
-
-    return maxProd;
+    return ans;
   }
 }
 
