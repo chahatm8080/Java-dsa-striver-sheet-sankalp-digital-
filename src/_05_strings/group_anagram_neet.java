b2c97ef52849eccae1785b7054bda478a57ce781
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-06-09 13:36:12 -0500

    optimal: creating fre[26] array

--- src/_05_strings/group_anagram_neet.java
+++ src/_05_strings/group_anagram_neet.java
@@ -1,4 +1,4 @@
-// import java.util.*;
+import java.util.*;
 
 // class Anagram {
 
@@ -48,98 +48,131 @@
 //     }
 // }
 
-// public class group_anagram_neet {
+class Anagram {
 
-//     public static void main(String[] args) {
+    private String getFrequencyString(String str) {
+        int[] freq = new int[26];
+        for (char c : str.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        StringBuilder frequencyString = new StringBuilder();
+        char ch = 'a';
+        for (int i : freq) {
+            frequencyString.append(ch);
+            frequencyString.append(i);
+        }
+        return frequencyString.toString();
+    }
 
-//         Scanner sc = new Scanner(System.in);
-
-//         int n = sc.nextInt();
-//         sc.nextLine();
-
-//         String[] strs = new String[n];
-
-//         for (int i = 0; i < n; i++) {
-//             strs[i] = sc.nextLine();
-//         }
-
-//         Anagram an = new Anagram();
-
-//         List<List<String>> ans = an.groupAnagrams(strs);
-
-//         System.out.println(ans);
-
-//         sc.close();
-//     }
-// }
-import java.util.*;
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0 || strs == null)
+            return new ArrayList<>();
+        Map<String, List<String>> mpp = new HashMap<>();
+        for (String str : strs) {
+            String Frequency = getFrequencyString(str);
+            if (mpp.containsKey(Frequency)) {
+                mpp.get(Frequency).add(str);
+            } else {
+                List<String> st = new ArrayList<>();
+                st.add(str);
+                mpp.put(Frequency, st);
+            }
+        }
+        return new ArrayList<>(mpp.values());
+    }
+}
 
 public class group_anagram_neet {
-
-    // Function to group anagrams using brute force
-    public static List<List<String>> groupAnagrams(String[] strs) {
-
-        List<List<String>> result = new ArrayList<>();
-        boolean[] visited = new boolean[strs.length];
-
-        for (int i = 0; i < strs.length; i++) {
-
-            if (visited[i]) {
-                continue;
-            }
-
-            List<String> group = new ArrayList<>();
-            group.add(strs[i]);
-            visited[i] = true;
-
-            // Compare current string with all remaining strings
-            for (int j = i + 1; j < strs.length; j++) {
-
-                if (visited[j]) {
-                    continue;
-                }
-
-                char[] arr1 = strs[i].toCharArray();
-                char[] arr2 = strs[j].toCharArray();
-
-                Arrays.sort(arr1);
-                Arrays.sort(arr2);
-
-                if (Arrays.equals(arr1, arr2)) {
-                    group.add(strs[j]);
-                    visited[j] = true;
-                }
-            }
-
-            result.add(group);
-        }
-
-        return result;
-    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of strings: ");
+        System.out.println(" enter the total string: ");
         int n = sc.nextInt();
+        sc.nextLine();
 
         String[] strs = new String[n];
-
-        System.out.println("Enter the strings:");
-
+        System.out.println("Enter the list of string");
         for (int i = 0; i < n; i++) {
             strs[i] = sc.next();
         }
 
-        List<List<String>> ans = groupAnagrams(strs);
+        Anagram an = new Anagram();
 
-        System.out.println("\nGrouped Anagrams:");
-
-        for (List<String> group : ans) {
-            System.out.println(group);
+        List<List<String>> ans = an.groupAnagrams(strs);
+        for (List<String> ls : ans) {
+            System.out.println(ls + " ");
         }
 
         sc.close();
     }
 }
+// import java.util.*;
+
+// public class group_anagram_neet {
+
+// public static List<List<String>> groupAnagrams(String[] strs) {
+
+// List<List<String>> result = new ArrayList<>();
+// boolean[] visited = new boolean[strs.length];
+
+// for (int i = 0; i < strs.length; i++) {
+
+// if (visited[i]) {
+// continue;
+// }
+
+// List<String> group = new ArrayList<>();
+// group.add(strs[i]);
+// visited[i] = true;
+
+// for (int j = i + 1; j < strs.length; j++) {
+
+// if (visited[j]) {
+// continue;
+// }
+
+// char[] arr1 = strs[i].toCharArray();
+// char[] arr2 = strs[j].toCharArray();
+
+// Arrays.sort(arr1);
+// Arrays.sort(arr2);
+
+// if (Arrays.equals(arr1, arr2)) {
+// group.add(strs[j]);
+// visited[j] = true;
+// }
+// }
+
+// result.add(group);
+// }
+
+// return result;
+// }
+
+// public static void main(String[] args) {
+
+// Scanner sc = new Scanner(System.in);
+
+// System.out.print("Enter number of strings: ");
+// int n = sc.nextInt();
+
+// String[] strs = new String[n];
+
+// System.out.println("Enter the strings:");
+
+// for (int i = 0; i < n; i++) {
+// strs[i] = sc.next();
+// }
+
+// List<List<String>> ans = groupAnagrams(strs);
+
+// System.out.println("\nGrouped Anagrams:");
+
+// for (List<String> group : ans) {
+// System.out.println(group);
+// }
+
+// sc.close();
+// }
+// }
