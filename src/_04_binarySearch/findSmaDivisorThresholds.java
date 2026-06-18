d1b2966bf3ad6b455c6d6dca6fb8f5e50e9d943e
CHAHAT MISHRA <chahatm8080@gmail.com>
2026-06-18 16:52:46 -0500

    optimal: using binary search

--- src/_04_binarySearch/findSmaDivisorThresholds.java
+++ src/_04_binarySearch/findSmaDivisorThresholds.java
@@ -1,6 +1,58 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// class Solution {
+//   public int smallestDivisor(int[] arr, int limit) {
+//     int n = arr.length;
+
+//     int max = Integer.MIN_VALUE;
+//     for (int num : arr) {
+//       max = Math.max(max, num);
+//     }
+
+//     for (int d = 1; d <= max; d++) {
+//       int sum = 0;
+//       for (int i = 0; i < n; i++) {
+//         sum += (int) Math.ceil((double) arr[i] / d);
+//       }
+
+//       if (sum <= limit) {
+//         return d;
+//       }
+//     }
+
+//     return -1;
+//   }
+// }
+
+// class findSmaDivisorThresholds {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int[] arr = new int[n];
+//     for (int i = 0; i < n; i++) {
+//       arr[i] = sc.nextInt();
+//     }
+//     int limit = 6;
+
+//     Solution obj = new Solution();
+//     int ans = obj.smallestDivisor(arr, limit);
+//     System.out.println("The minimum divisor is: " + ans);
+//     sc.close();
+//   }
+// }
+
+import java.util.*;
 
 class Solution {
+  public int findSmThreshold(int[] arr, int d) {
+    int n = arr.length;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += (int) Math.ceil((double) arr[i] / d);
+    }
+    return sum;
+  }
+
   public int smallestDivisor(int[] arr, int limit) {
     int n = arr.length;
 
@@ -8,19 +60,19 @@ class Solution {
     for (int num : arr) {
       max = Math.max(max, num);
     }
-
-    for (int d = 1; d <= max; d++) {
-      int sum = 0;
-      for (int i = 0; i < n; i++) {
-        sum += (int) Math.ceil((double) arr[i] / d);
-      }
-
-      if (sum <= limit) {
-        return d;
+    int low = 0;
+    int high = max;
+    int ans = -1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (findSmThreshold(arr, mid) <= limit) {
+        ans = mid;
+        high = mid - 1;
+      } else {
+        low = mid + 1;
       }
     }
-
-    return -1;
+    return ans;
   }
 }
 
